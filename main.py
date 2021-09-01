0a86b83f913b89dcb4c5e08d265f2af5b02e1b2f
Jason Trevor <thies.jason@gmx.de>
2021-09-01 08:22:22 +0200

    - removed class to allow for st.cache improvements

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from app import page_configuration, introduction, training_results, interactive_section, show_result
-from sentiment_analyzer import SentimentModel
+from sentiment_analyzer import get_model, get_sentiment
 
 if __name__ == '__main__':
     page_configuration()
@@ -10,8 +10,8 @@ if __name__ == '__main__':
 
     user_input, model_type = interactive_section()
 
-    model = SentimentModel(model_type=model_type)
+    model = get_model(model_type=model_type)
 
-    result = model.get_sentiment(user_input=user_input)
+    result = get_sentiment(model, model_type=model_type, user_input=user_input)
 
     show_result(model_type, user_input, result)
--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -9,23 +9,23 @@ class Sentiment(Enum):
     Neutral = "neutral"
 
 
-class SentimentModel:
+@st.cache
+def get_model(model_type: str):
+    assert model_type in ["VADER"]
 
-    def __init__(self, model_type: str):
-        assert model_type in ["VADER"]
-        self.model_type = model_type
+    if model_type == "VADER":
+        return SentimentIntensityAnalyzer()
 
-        if model_type == "VADER":
-            self.model = SentimentIntensityAnalyzer()
 
-    def get_sentiment(self, user_input: str):
-        if self.model_type == "VADER":
-            output_dict = self.model.polarity_scores(user_input)
+@st.cache
+def get_sentiment(model, model_type: str, user_input: str):
+    if model_type == "VADER":
 
-            sentiment_dic = {Sentiment.Positive: output_dict["pos"], Sentiment.Negative: output_dict["neg"],
-                             Sentiment.Neutral: output_dict["neu"]}
+        output_dict = model.polarity_scores(user_input)
+        sentiment_dic = {Sentiment.Positive: output_dict["pos"], Sentiment.Negative: output_dict["neg"],
+                         Sentiment.Neutral: output_dict["neu"]}
 
-            return max(sentiment_dic, key=sentiment_dic.get).value
+        return max(sentiment_dic, key=sentiment_dic.get).value
 
-        else:
-            assert self.model_type == "VADER"
+    else:
+        assert model_type == "VADER"
